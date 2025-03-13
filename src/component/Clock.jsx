8495ec7a3f3883b67598a131a2f96ac5add8cc30
khang <khangcaopham2003@gmail.com>
2025-03-13 15:30:56 +0700

    thêm số và css

--- src/component/Clock.jsx
+++ src/component/Clock.jsx
@@ -26,29 +26,38 @@ const Clock = () => {
     // Xóa interval khi component unmount
     return () => clearInterval(interval);
   }, []);
-
-  //Tính toán góc quay của các kim đồng hồ:
-  // Mỗi giờ: Số giờ quay 360 độ / 12h = 30 độ + (phút/60) * 30 độ
-  // Mỗi phút: Số phút quay 360 độ / 60p = 6 độ + (giây/60) * 6 độ
-  // Mỗi giây: Số giây quay 360 độ / 60s = 6 độ
-  // Ví dụ: 3h 15p 30s
-  // Kim giờ: 3 * 30 + (15/60) * 30 = 97.5 độ
-  // Kim phút: 15 + (30/60) * 6 = 15 + 3 = 18 độ
+  /*
+  Tính toán góc quay của các kim đồng hồ:
+  Mỗi giờ: Số giờ quay 360 độ / 12h = 30 độ + (phút/60) * 30 độ
+  Mỗi phút: Số phút quay 360 độ / 60p = 6 độ + (giây/60) * 6 độ
+  Mỗi giây: Số giây quay 360 độ / 60s = 6 độ
+  Ví dụ: 3h 15p 30s
+  Kim giờ: 3 * 30 + (15/60) * 30 = 97.5 độ
+  Kim phút: 15 + (30/60) * 6 = 15 + 3 = 18 độ
+  */
   const hourHand = (time.hour % 12) * 30 + (time.minute / 60) * 30;
   const minuteHand = (time.minute + time.second / 60) * 6;
   const secondHand = time.second * 6;
 
   // Tạo danh sách các vạch giờ (12 vạch)
   const hourLine = [];
-  for (let i = 0; i < 12; i++) {
+  for (let i = 0; i < 60; i += 5) {
     hourLine.push(
       <div
+        className="line-container"
         key={i}
-        style={{
-          transform: `rotate(${i * 30}deg)`,
-        }}
-        className={`line`}
-      ></div>
+        style={{ transform: `rotate(${i * 6}deg)` }}
+      >
+        {i % 15 === 0 ? (
+          <div className="line-number">{i === 0 ? "12" : i / 5}</div>
+        ) : (
+          ""
+        )}
+        <div
+          className="line"
+          style={{ backgroundColor: i % 15 !== 0 ? "black" : "" }}
+        ></div>
+      </div>
     );
   }
 
